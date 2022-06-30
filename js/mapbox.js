ba176a3e2ff50b7865e8d24d60f42f0dee815046
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-06-30 11:54:08 -0500

    Added first marker on map

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,10 +1,18 @@
 'use strict'
-
+// Setting token
 mapboxgl.accessToken = mapBoxToken;
 
+//Creating map, attaching it to div with map ID
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [-122.330062, 47.603832], // starting position [lng, lat]
-    zoom: 5 // starting zoom
+    zoom: 10 // starting zoom
 });
+
+//Captures where user double clicks, still zooms..
+map.on('dblclick', (e) => {
+    console.log(`User clicked at ${e.lngLat}`)
+})
+
+let favRestaraunt = new mapboxgl.Marker().setLngLat([-122.313117,47.668191]).addTo(map)
