b712e30fb60a1cbe5b4d9d8c5e0d103c64eb8459
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-06-30 11:26:40 -0500

    added geocode functions, used to set initial map position to Seattle

--- js/mapbox.js
+++ js/mapbox.js
@@ -1,9 +1,10 @@
 'use strict'
 
 mapboxgl.accessToken = mapBoxToken;
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: [-122.330062, 47.603832], // starting position [lng, lat]
+    zoom: 5 // starting zoom
 });
